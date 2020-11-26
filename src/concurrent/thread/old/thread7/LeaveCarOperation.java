f6a635f8e35629e4f12fad9bad8dd74732ddbb73
albert.fang <albert.fang@51job.com>
2020-11-26 15:32:58 +0800

    add
    移动2019年练习线程包

---
+++ src/concurrent/thread/old/thread7/LeaveCarOperation.java
@@ -0,0 +1,20 @@
+package concurrent.thread.old.thread7;
+
+/**
+ * @ClassName: LeaveCarOperation
+ * @Description: description of class
+ * @Author: Albert
+ * @CreateDate: 2020/1/7 9:27
+ */
+public class LeaveCarOperation implements Runnable{
+    private Carport carport;
+    public LeaveCarOperation(Carport carport){
+        this.carport = carport;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            carport.leave();
+        }
+    }
+}
