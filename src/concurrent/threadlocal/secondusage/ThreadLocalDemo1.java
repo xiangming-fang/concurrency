cf248b7d8afc23797eccbcd60748224f6de516a6
albert.fang <albert.fang@51job.com>
2020-11-30 16:08:56 +0800

    record this time
    记录这个有意义的时刻，我终于学会了如何在idea里合并两个分支了

--- src/concurrent/threadlocal/secondusage/ThreadLocalDemo1.java
+++ src/concurrent/threadlocal/secondusage/ThreadLocalDemo1.java
@@ -1,10 +1,23 @@
 package concurrent.threadlocal.secondusage;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author: albert.fang
  * @date: 2020/11/27 18:54
  * @description: threadlocal的第二种用法示例
  */
 public class ThreadLocalDemo1 {
-    
+    public static void main(String[] args) {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+//        for (int i = 0; i < 10; i++) {
+            fixedThreadPool.submit(() -> {
+                for (int j = 0; j < 100; j++) {
+                    System.out.printf("【%s】--- execute tasks\n",Thread.currentThread().getName());
+                }
+            });
+//        }
+        fixedThreadPool.shutdown();
+    }
 }
